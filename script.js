66d831c41758a1c185b9aecccaf214f0ec0c10ae
Kanhaiyaa <128280779+Kanhaiyaa@users.noreply.github.com>
2024-01-31 11:11:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 
-// const confirmation=confirm("Do you want to proceed?")
+const confirmation=confirm("Do you want to proceed?")
 
 if (confirmation) {
 	alert( "You clicked OK. Proceeding...")
